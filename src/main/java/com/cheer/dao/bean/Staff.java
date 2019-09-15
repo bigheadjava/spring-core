cb5d6f36af6b90a59944cdbd736a509e1da365ec
yinchu <javayinchu2013@163.com>
2019-09-15 11:05:13 +0800

    updated

---
+++ src/main/java/com/cheer/dao/bean/Staff.java
@@ -0,0 +1,55 @@
+package com.cheer.dao.bean;
+
+public class Staff {
+
+	private int id;
+
+	private String name;
+
+	private int age;
+
+	private double salary;
+
+	public Staff() {
+
+	}
+
+	public Staff(String name, int age, double salary) {
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
